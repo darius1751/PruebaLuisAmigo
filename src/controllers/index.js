0a43e96ff819f276224e33ea198ba6a4094fbde6
darius1751 <lupequi12@gmail.com>
2021-07-18 13:13:09 -0500

    Algunas configuraciones sobre las funciones de ordenar

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,23 +2,54 @@ const get = helperFecthApi();
 const app = new Vue({
     el:"#app",
       data() {
-          return {
-              items: []
+        return {
+            ordenAZ:true,
+            items: []
         }
       },
       created:function(){
         const getData = async ()=>{
             this.$data.items = await get('https://ghibliapi.herokuapp.com/films/',{'content-type':'application/json'});
-            console.log(this.$data.items);
             this.$data.items = this.$data.items.map(data=>{
                 const {title,original_title,producer,director} = data;
                 return {title,original_title,producer,director};
             });
+            this.$data.items = this.ordenarA_Z;
         }
         getData();
-        
-        
-      }
+      },
+        computed:{
+            ordenarZ_A:function(){
+                let datos= this.$data.items;
+                for(let i = 0; i < datos.length-1;i++){
+                    let cambio = false;
+                    for(let i = 0; i < datos.length-1;i++){
+                        if(datos[i].title < datos[i+1].title){
+                            aux = datos[i+1];
+                            datos[i+1] = datos[i];
+                            datos[i] = aux;
+                            cambio = true;
+                        }
+                    }
+            
+                }
+                return datos;
+              },
+              ordenarA_Z:function(){
+                let datos= this.$data.items;
+                for(let i = 0; i < datos.length-1;i++){
+                    for(let i = 0; i < datos.length-1;i++){
+                        if(datos[i].title > datos[i+1].title){
+                            aux = datos[i+1];
+                            datos[i+1] = datos[i];
+                            datos[i] = aux;
+                        }
+                    }
+                
+                }
+                return datos;
+              }
+        }
       
   }
   )
